168d0a6eec69a93ba5147072cdb634acd3999307
Cris Daniluk <cdaniluk@rhythmictech.com>
2020-11-12 10:54:20 -0500

    bump module version

--- main.tf
+++ main.tf
@@ -134,7 +134,7 @@ resource "aws_launch_configuration" "this" {
 
 module "bitbucketdb" {
   source  = "rhythmictech/rds-postgres/aws"
-  version = "4.1.0-rc1"
+  version = "4.1.0-rc2"
 
   name                    = local.db_name
   allowed_cidr_blocks     = var.db_allowed_access_cidrs
